4f1c8dd15a813d27abcd26af5d6caa341ad62559
rongye chen <chenrongye@sina.com>
2012-04-11 12:50:00 +0800

    bug

--- phprender.class.php
+++ phprender.class.php
@@ -1,11 +1,13 @@
 <?php
 /**
+ * @todo a php template engine
+ * @version 1.0.2
  * @author rongye@staff.sina.com.cn
+ * @date 2012.4.4
  * 用法：
  * 模板参考模板  "./templates/index.html"
- * include_once 'template.class.php';
- * $render = new Render('templates');
- * $render->setCaching(true);
+ * include_once 'render.class.php';
+ * $render = new Render('tmpl');
  * $variable = array('a'=>8,'b'=>4,'c'=>8,'name'=>'新浪无线','user'=>array('a','b','c','d','e','f','g'));
  * $render->index($variable);
  * */
@@ -13,21 +15,10 @@ class Render{
 	
 	private $path = '';
 	private $tmplname = '';
-	private $caching = false;
-	private $memcache = null;
 	
 	public function __construct($path)
 	{
 		$this->path = $path;
-		$this->memcache = memcache_init();
-
-	}
-	public function __destruct()
-	{         
-	}	
-	public function setCaching($status)
-	{		
-		$this->caching= $status;
 	}
 	
 	private function pre_process($html)
@@ -51,41 +42,12 @@ class Render{
 	
 	private function getfile($fileName)
 	{
-		if($this->caching)
-		{
-			if(!($html = memcache_get($this->memcache,md5($fileName))))
-			{
-				$html =  file_get_contents($fileName);
-				memcache_set($this->memcache,md5($fileName),$html);
-			}
-		}
-		else
-		{
-			$html =  file_get_contents($fileName);
-		}
-		return $html;
+		return file_get_contents($fileName);
 	}
 	
 	private function parse($parameters)
 	{		
-		if($this->caching && memcache_get($this->memcache,md5($this->tmplname.'.func')) )
-		{
-			$nfunc = $this->bulidTmplFunc('');
-			return implode("\n", $nfunc($parameters));
-		}
-		if($this->caching)
-		{
-			$html = memcache_get($this->memcache,md5($this->tmplname.'.html'));
-			if(!$html)
-			{
-				$html = $this->getTmpl($this->tmplname);
-				memcache_set($this->memcache,md5($this->tmplname.'.html'),$html);
-			}
-		}
-		else
-		{
-			$html = $this->getTmpl($this->tmplname);
-		}
+		$html = $this->getTmpl($this->tmplname);
 		$nfunc = $this->bulidTmplFunc(explode('\n',$html));
 		return implode("\n", $nfunc($parameters));	
 	}
@@ -95,21 +57,12 @@ class Render{
 		$html = preg_replace('/\r\n/','\n',$html);
 		$html = preg_replace('/\r/','\n',$html);
 		$html = preg_replace('/\n/','\n',$html);
-		//$html = preg_replace('/"/','\\"',$html);
 		$html = preg_replace("/'/","\'",$html);
 		return $html;
 	}
 
 	private function bulidTmplFunc($lines)
 	{
-		if($this->caching)
-		{
-			$func = memcache_get($this->memcache,md5($this->tmplname.'.func'));
-			if($func)
-			{
-				return create_function('$parameters',$func);
-			}	
-		}	
 		$func = 'extract($parameters);$__=array();';
 		$patt = '/\{\{(\/?)(\w+)(.*?)\}\}/';
 		for($i=0;$i<count($lines);$i++)
@@ -128,7 +81,7 @@ class Render{
 					}
 					else
 					{
-						$func .= $matches[2].$matches[3] ;
+						$func .= stripslashes($matches[2].$matches[3]);
 					}						
 					$func .= "{";
 				}	
@@ -140,26 +93,19 @@ class Render{
 			else
 			{
 				$lines[$i] = preg_replace('/\${(.*?)}/','\'.\$$1.\'',$lines[$i]);
-				$func .= '$__[] = \''.$lines[$i].'\';';
+				$func .= '$__[] = \''.stripslashes($lines[$i]).'\';';
 			}
 		}			
 		$func  .= 'return $__;';
-		//echo $func;exit;
-		if($this->caching)
-		{	
-			memcache_set($this->memcache,md5($this->tmplname.'.func'),$func);
-		}
 		return create_function('$parameters',$func);	
 	}
 	public function __call($name,$arg)
 	{
 		$this->tmplname = $name;
 		$html = $this->parse($arg[0]);
-        //Header("Content-type: text/html;charset=utf-8");
         Header("Content-type: text/html");
 	    Header("Cache-Control: no-cache, must-revalidate");
 	    Header("Pragma: no-cache");	
-	    //$html = stripslashes($html);
 	    echo $html;		
 	}
 }
